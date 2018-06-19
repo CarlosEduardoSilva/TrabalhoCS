409e1ff6f4c517942bf970c70835a4cd4eeae4bc
Carlos Eduardo <cadu.eduardo1@outlook.com>
2018-06-19 10:02:42 -0300

    Adicionados nossos metodos Get e Set

--- Trabalho_CS_Final/Controllers/ControleCliente.cs
+++ Trabalho_CS_Final/Controllers/ControleCliente.cs
@@ -8,5 +8,8 @@ namespace Controllers
 {
     class ControleCliente
     {
+
+
+
     }
 }
--- Trabalho_CS_Final/Models/ModelCliente.cs
+++ Trabalho_CS_Final/Models/ModelCliente.cs
@@ -8,5 +8,17 @@ namespace Models
 {
     class ModelCliente
     {
+        public int ID_Cliente { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Login { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Email { get; set; }
+
+        public int ID_Plano { get; set; }
+
     }
 }

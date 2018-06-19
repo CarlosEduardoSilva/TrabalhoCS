2b0018c660e00232a2c3251b7a0af3cb0b759dc9
Carlos Eduardo <cadu.eduardo1@outlook.com>
2018-06-19 10:08:26 -0300

    Criada nossa BaseManager <T>

---
+++ Trabalho_CS_Final/Controllers/BaseManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    interface BaseManager<T> where T : class
+    {
+        void Salvar(T entidade);
+        void Atualizar(T entidade);
+        void Excluir(int id);
+        T Buscar(int id);
+        List<T> BuscarTodos();
+    }
+}

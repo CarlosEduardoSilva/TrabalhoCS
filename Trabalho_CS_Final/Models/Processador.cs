d5d46f686c7330f89aa895711ef4470cdf0210a5
Richard Teske <richard25sa@gmail.com>
2018-06-25 08:58:40 -0300

    Criada a classe Processador

---
+++ Trabalho_CS_Final/Models/Processador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class Processador
+    {
+
+        public int ID_Processador { get; set; }
+
+        public int Nome { get; set; }
+
+        public decimal Valor { get; set; }
+
+    }
+}

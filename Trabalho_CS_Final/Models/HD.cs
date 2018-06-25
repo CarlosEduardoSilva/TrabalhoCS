630f2ec902df854ebc0ca53fa5d19a313fd2fefc
Richard Teske <richard25sa@gmail.com>
2018-06-25 09:29:08 -0300

    Terminando as models com [Key] e inicializando as controllers

--- Trabalho_CS_Final/Models/HD.cs
+++ Trabalho_CS_Final/Models/HD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,9 @@ namespace Models
 {
     class HD
     {
-
+        [Key]
         public int ID_HD { get; set; }
-
         public string Nome { get; set; }
-
         public decimal Valor { get; set; }
 
     }
